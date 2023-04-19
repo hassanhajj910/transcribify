f00db83398a4d95019e6d78e2d004195d67bce4f
Hassan El-Hajj <hhajj@mpiwg-berlin.mpg.de>
2023-04-19 16:49:53 +0200

    added args

--- transcribify.py
+++ transcribify.py
@@ -1,7 +1,14 @@
 import transcription_utils
 import torchaudio
+import argparse
 
-audio_test = transcription_utils.audio('data/weights_1.wav')
+parser = argparse.ArgumentParser(description="pass files")
+parser.add_argument('-i', '--input', type=str,  help='Path to the WAV file', required=True)
+parser.add_argument('-o', '--output', type=str, help='Path to output text file', required=True)
+args = vars(parser.parse_args())
+
+
+audio_test = transcription_utils.audio(args['input'])
 print('step 1')
 times = audio_test.get_clean_sections(threshold=5)
 print('step 2')
@@ -11,6 +18,6 @@ clean, rate = audio_test.clean_audio(times)
 print('step 4')
 speaker_times = audio_test.get_speaker_sections()
 print('step 5')
-audio_test.transcribe(speaker_times, 'data/weights_fullres.txt')
+audio_test.transcribe(speaker_times, args['output'])
 print('done')
 
