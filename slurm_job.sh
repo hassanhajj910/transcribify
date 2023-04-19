2136be4273c822435104d9f0254f202f33584bbe
Hassan El-Hajj <hhajj@mpiwg-berlin.mpg.de>
2023-04-19 18:46:38 +0200

    .

--- slurm_job.sh
+++ slurm_job.sh
@@ -5,6 +5,7 @@
 #SBATCH --mem=50G
 #SBATCH -o outfile.%J
 #SBATCH --mail-user=hhajj@mpiwg-berlin.mpg.de
+module load anaconda3
 source $ANACONDA3_ROOT/etc/profile.d/conda.sh
 conda activate ml_audio_small
 srun python transcribify.py -i 'podcast_ep1.wav' -o 'podcast_ep1.txt'
