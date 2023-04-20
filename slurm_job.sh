4e474caed0bc36bfbff07524db54b271b2662929
Hassan El-Hajj <hhajj@mpiwg-berlin.mpg.de>
2023-04-20 15:45:09 +0200

    .

--- slurm_job.sh
+++ slurm_job.sh
@@ -9,4 +9,4 @@
 module load anaconda3
 source $ANACONDA3_ROOT/etc/profile.d/conda.sh
 conda activate ml_audio_small
-srun python transcribify.py -i 'podcast_ep1.wav' -o 'podcast_ep1.txt'
+srun python transcribify.py -i 'weights_small.wav' -o 'weights_small.txt'
