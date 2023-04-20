5e68a4b8e78d604c0650166d3b27ef8da5845417
Hassan El-Hajj <hhajj@mpiwg-berlin.mpg.de>
2023-04-20 10:27:34 +0200

    slurm timelimit

--- slurm_job.sh
+++ slurm_job.sh
@@ -5,6 +5,7 @@
 #SBATCH --mem=50G
 #SBATCH -o outfile.%J
 #SBATCH --mail-user=hhajj@mpiwg-berlin.mpg.de
+#SBATCH -t 12:00:00
 module load anaconda3
 source $ANACONDA3_ROOT/etc/profile.d/conda.sh
 conda activate ml_audio_small
--- transcribify.py
+++ transcribify.py
@@ -1,12 +1,14 @@
 import transcription_utils
 import torchaudio
 import argparse
+import time
 
 parser = argparse.ArgumentParser(description="pass files")
 parser.add_argument('-i', '--input', type=str,  help='Path to the WAV file', required=True)
 parser.add_argument('-o', '--output', type=str, help='Path to output text file', required=True)
 args = vars(parser.parse_args())
 
+start_time = time.time()
 
 audio_test = transcription_utils.audio(args['input'])
 print('step 1')
@@ -19,5 +21,8 @@ print('step 4')
 speaker_times = audio_test.get_speaker_sections()
 print('step 5')
 audio_test.transcribe(speaker_times, args['output'])
-print('done')
+end_time = time.time()
+process_time = end_time - start_time
+process_time = process_time/60
+print('done - process took {} minutes'.format(process_time))
 
