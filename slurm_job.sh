34c53b0a23eafaa1181aa6b331739690758bd220
Hassan El-Hajj <hhajj@mpiwg-berlin.mpg.de>
2023-04-21 14:33:10 +0200

    strucutral change

--- slurm_job.sh
+++ slurm_job.sh
@@ -9,4 +9,4 @@
 module load anaconda3
 source $ANACONDA3_ROOT/etc/profile.d/conda.sh
 conda activate ml_audio_small
-srun python transcribify.py -i 'podcast_ep1.wav' -o 'podcast_ep1.txt'
+srun python transcribify.py -i 'data/mpiwg_podcast' -o 'results/mpiwg_podcast'
--- transcribify.py
+++ transcribify.py
@@ -2,27 +2,47 @@ import transcription_utils
 #import torchaudio
 import argparse
 import time
+import os
+import re
 
 parser = argparse.ArgumentParser(description="pass files")
-parser.add_argument('-i', '--input', type=str,  help='Path to the WAV file', required=True)
-parser.add_argument('-o', '--output', type=str, help='Path to output text file', required=True)
+parser.add_argument('-i', '--input', type=str,  help='Path to input folder with WAV file', required=True)
+parser.add_argument('-o', '--output', type=str, help='Path to output folder to store text file', required=True)
 args = vars(parser.parse_args())
 
+RE_EXP = '^(.+)\.wav$'
+ 
 start_time = time.time()
+allwav = os.listdir(args['input'])
 
-audio_test = transcription_utils.audio(args['input'])
-print('step 1')
-times = audio_test.get_clean_sections(threshold=5)
-print('step 2')
-mapper = audio_test.time_mapper(times)
-print('step 3')
-clean, rate = audio_test.clean_audio(times)
-print('step 4')
-speaker_times = audio_test.get_speaker_sections()
-print('step 5')
-audio_test.transcribe(speaker_times, args['output'])
+for wavfile in allwav:
+    print('-- Processing file: {}'.format(wavfile))
+    iter_start = time.time()
+    fpath = os.path.join(args['input'], wavfile)
+    audio_test = transcription_utils.audio(fpath)
+    print('step 1')
+    times = audio_test.get_clean_sections(threshold=5)
+    print('step 2')
+    mapper = audio_test.time_mapper(times)
+    print('step 3')
+    clean, rate = audio_test.clean_audio(times)
+    print('step 4')
+    #speaker_times = audio_test.get_speaker_sections()
+    print('step 5')
+
+    se = re.search(RE_EXP, wavfile)
+    textname = str(se.group(1)) + '.txt'
+    textname = os.path.join(args['output'], textname)
+    print(textname)
+    #audio_test.transcribe(speaker_times, args['output'])
+    iter_end = time.time()
+    iter_time = iter_end - iter_start
+    iter_time = iter_time/60
+    print('------ Processed file: {} in {} minutes'.format(wavfile, iter_time))
+
+
 end_time = time.time()
 process_time = end_time - start_time
 process_time = process_time/60
-print('done - process took {} minutes'.format(process_time))
+print('Processing completed in {} minutes'.format(process_time))
 
